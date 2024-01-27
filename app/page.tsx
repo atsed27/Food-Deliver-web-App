efdc9413006f19636ec08a8ea603a2afff08272e
atsed27 <danielnigatu09@gmail.com>
2024-01-27 03:32:52 -0300

    best component is done

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import Best from '@/components/Best';
+
 export default async function Home() {
   return (
     <main>
-      <div className="text-rose-700">Daniel</div>
+      <Best />
     </main>
   );
 }

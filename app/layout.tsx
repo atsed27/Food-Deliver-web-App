24fad593f1693774e68126847c755964532b4c62
atsed27 <danielnigatu09@gmail.com>
2024-01-30 06:39:09 -0300

    footer is done

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google';
 import './globals.css';
 import Notification from '@/components/Notification';
 import NavBar from '@/components/NavBar';
+import Footer from '@/components/Footer';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -22,6 +23,7 @@ export default function RootLayout({
         <Notification />
         <NavBar />
         {children}
+        <Footer />
       </body>
     </html>
   );
---
+++ components/Footer.tsx
@@ -0,0 +1,78 @@
+import Link from 'next/link';
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="flex flex-col items-center justify-center bg-primary mt-10 md:mt-20  px-3 md:px-10 pt-7 xl:px-20 ">
+      <div className="w-full ">
+        <div className="grid grid-cols-2 gap-4 mx-3 my-2 jioFooter md:grid-cols-4">
+          <div className="">
+            <h2 className="mb-4 text-xl font-bold md:text-2xl">
+              <span className="bg-green-500 text-white px-2 rounded-md ">
+                D
+              </span>
+              oro
+            </h2>
+            <p className="">
+              Lorem ipsum dolor sit amet,consectetur adipiscing elit.
+            </p>
+          </div>
+          <div className="">
+            <h2 className="mb-4 text-xl font-bold ">Useful links</h2>
+            <div className="">
+              <Link href={'/'}>
+                <h2 className="py-1"> About us</h2>
+              </Link>{' '}
+              <Link href={'/'}>
+                <h2 className="py-1"> Events</h2>
+              </Link>
+              <Link href={'/'}>
+                <h2 className="py-1"> Blogs</h2>
+              </Link>
+              <Link href={'/'}>
+                <h2 className="py-1"> FAQ</h2>
+              </Link>
+            </div>
+          </div>
+          <div>
+            <h2 className="mb-4 text-xl font-bold">Main Menu</h2>
+            <div className="">
+              <Link href={'/'}>
+                <h2 className="py-1"> Home</h2>
+              </Link>{' '}
+              <Link href={'/'}>
+                <h2 className="py-1"> Offers</h2>
+              </Link>
+              <Link href={'/'}>
+                <h2 className="py-1"> Menu</h2>
+              </Link>
+              <Link href={'/'}>
+                <h2 className="py-1"> Reservation</h2>
+              </Link>
+            </div>
+          </div>
+          <div>
+            <h2 className="mb-4 text-xl font-bold">Contact Us</h2>
+            <div className="">
+              <Link href={'/'}>
+                <h2 className="py-1">danielnigtu09@gmail.com</h2>
+              </Link>{' '}
+              <Link href={'/'}>
+                <h2 className="py-1">+251 916 213 371</h2>
+              </Link>
+              <Link href={'/'}>
+                <h2 className="py-1">social media</h2>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="my-4 flex flex-col sm:flex-row items-center ">
+        <div className="mr-5">Social media </div>
+        <h2 className="text-lg">Copyright @ 2023 melhk | All right reserved</h2>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
